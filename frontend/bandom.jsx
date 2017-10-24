584df56a56ce7b70fe592e4cbf8d164e94812ab8
Max <xamskier@aol.com>
2017-10-24 17:10:22 -0400

    bug fix?

--- frontend/bandom.jsx
+++ frontend/bandom.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
-import configureStore from './store/store';
+// import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
   const root = document.addElementById("root");
   ReactDOM.render(<h1>BANDOM</h1>, root);
 });
