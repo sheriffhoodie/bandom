553d0a82438cf7632e98a677ebf1f3e112c42842
Max <xamskier@aol.com>
2017-11-01 11:26:58 -0400

    more user bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   def logout
     current_user.reset_session_token!
     session[:session_token] = nil
+    @current_user = nil
   end
 
   def logged_in?
@@ -24,7 +25,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    return nil unless session[:session_token]
+    # return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
