497868728ea26a57dbbfce140a498f20680ffe78
Max <xamskier@aol.com>
2017-11-01 11:29:15 -0400

    MORE user model bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    # return nil unless session[:session_token]
+    return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
