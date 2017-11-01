3b15466a534ef8d907f9f2d069cfb67365043eb4
Max <xamskier@aol.com>
2017-11-01 11:22:59 -0400

    user bang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
 
   def login(user)
     @current_user = user
-    session[:session_token] = user.reset_session_token
+    session[:session_token] = user.reset_session_token!
   end
 
   def logout
-    current_user.reset_session_token
+    current_user.reset_session_token!
     session[:session_token] = nil
   end
 
