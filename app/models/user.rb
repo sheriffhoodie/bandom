a7bdfb1831fda290d414aadb6a45af50917543c6
Max <xamskier@aol.com>
2017-11-01 11:16:09 -0400

    user model bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,48 +17,49 @@
 #
 
 class User < ApplicationRecord
-validates :username, :password_digest, :session_token, presence: true, uniqueness: true
-validates :password, length: { minimum: 6, allow_nil: true }
-has_attached_file :image, default_url: "default-avatar.png"
-validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  has_attached_file :image, default_url: "default-avatar.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-has_many :albums, dependent: :destroy,
-foreign_key: :artist_id,
-class_name: :Album
+  has_many :albums, dependent: :destroy,
+  foreign_key: :artist_id,
+  class_name: :Album
 
-has_many :tracks,
-through: :albums,
-source: :tracks
+  has_many :tracks,
+  through: :albums,
+  source: :tracks
 
-before_validation :ensure_session_token
+  before_validation :ensure_session_token
 
-attr_reader :password
+  attr_reader :password
 
-def self.find_by_credentials(username, password)
-  user = User.find_by(username: username)
-  user && user.is_password?(password) ? user : nil
-end
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
 
-def self.generate_session_token
-  SecureRandom.urlsafe_base64
-end
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
 
-def reset_session_token
-  self.session_token = User.generate_session_token
-  self.save!
-  self.session_token
-end
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
 
-def is_password?(password)
-  BCrypt::Password.new(self.password_digest).is_password?(password)
-end
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
-def password=(password)
-  @password = password
-  self.password_digest = BCrypt::Password.create(password)
-end
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
-def ensure_session_token
-  self.session_token ||= User.generate_session_token
-end
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
