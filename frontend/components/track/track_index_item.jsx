de73008df28fc78f26957525c99923580d7a1b60
Max <xamskier@aol.com>
2017-11-03 12:04:10 -0400

    functional audio player with HTML5

--- frontend/components/track/track_index_item.jsx
+++ frontend/components/track/track_index_item.jsx
@@ -11,8 +11,15 @@ class TrackIndexItem extends React.Component {
     const track = this.props.track;
     return (
       <div>
+      <div>
         <p>{track.ord}. {track.title}</p>
       </div>
+      <div>
+        <audio controls preload="metadata">
+        	<source src={"https://s3.us-east-2.amazonaws.com/bandom-dev/my-audio/Desiigner+-+Panda+(Remix+Cover).mp3"} type="audio/mp3"/>
+        </audio><br />
+      </div>
+    </div>
     );
   }
 }
