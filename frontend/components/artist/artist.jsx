22af1289589e4d74bbed6d1a3748472016f643f6
Max <xamskier@aol.com>
2017-12-10 16:34:30 -0500

    added artist page styling

--- frontend/components/artist/artist.jsx
+++ frontend/components/artist/artist.jsx
@@ -14,12 +14,23 @@ class Artist extends React.Component {
     if (this.props.artist && this.props.artist.albums) {
       const albums = Object.keys(this.props.artist.albums);
       return (
-        <div className="artist-show-main">
-          <h2>{this.props.artist.artistName}</h2>
+        <div className="main">
+          <div className="album-show-main">
+          <div className="cover-div">
+            <p className="cover-title">{this.props.artist.artistName}</p>
+          </div>
+        <div className="artist-show-body">
+          <div className="artist-info">
           <img className="artist-pic" src={this.props.artist.image_url}></img>
-          <h5>Location: {this.props.artist.location}</h5>
+          <div className="artist-inner-info">
+          <h2>{this.props.artist.artistName}</h2>
+          <h4 className="location">Location: {this.props.artist.location}</h4>
+          </div>
+        </div>
+        <div className="discog-div">
+
           <h4 className="discog-header">Discography</h4>
-          <div className="artist-info-div">
+          <div className="artist-discog-div">
             <ul className="albums-list">
               {albums.map(id => (
                 <div className="album-item">
@@ -34,21 +45,24 @@ class Artist extends React.Component {
               ))}
             </ul>
           </div>
-          <footer className="footer">
-            <p className="footer-text">
-              Follow me for more!
-            </p>
-              <a href="http://github.com/sheriffhoodie">
-                <i className="icon fa fa-github-square fa-3x" aria-hidden="true"></i>
-              </a>
-              <a href="https://www.linkedin.com/in/maxwell-currier-a7769263/">
-                <i className="icon fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
-              </a>
-              <a href="https://angel.co/maxwell-currier">
-                <i className="icon fa fa-angellist fa-3x" aria-hidden="true"></i>
-              </a>
-          </footer>
+          </div>
         </div>
+        </div>
+        <footer className="footer">
+          <p className="footer-text">
+            Follow me for more!
+          </p>
+          <a href="http://github.com/sheriffhoodie">
+            <i className="icon fa fa-github-square fa-3x" aria-hidden="true"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/maxwell-currier-a7769263/">
+            <i className="icon fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
+          </a>
+          <a href="https://angel.co/maxwell-currier">
+            <i className="icon fa fa-angellist fa-3x" aria-hidden="true"></i>
+          </a>
+        </footer>
+      </div>
       );
     } else {
       return null;
