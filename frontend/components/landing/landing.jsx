8f11f1e00caaacad5b7148b5fbeee8c596ae24ed
Max <xamskier@aol.com>
2018-02-24 18:48:00 -0500

    landing css overhaul

--- frontend/components/landing/landing.jsx
+++ frontend/components/landing/landing.jsx
@@ -18,43 +18,49 @@ class Landing extends React.Component {
     return (
       <div className="landing-main">
         <div className="landing-features">
-          <div className="landing-main-feature">
+          <div className="main-feature-outer">
             <Link to={`/users/${featureds[1]}`}>
-            <img src="https://s3.us-east-2.amazonaws.com/bandom-dev/featured-artist-photos/sub-feature1.jpg"
-              className="landing-mainfeat-headline"></img>
+            <div className="main-feature-inner">
+              <img src="https://s3.us-east-2.amazonaws.com/bandom-dev/featured-artist-photos/sub-feature1.jpg"
+                className="main-feature-img"></img>
               <p className="main-headline">
-                Kids By The Sink: Washing Their Hands Of Mediocrity
-                <br/>
-                With New Album
+                Kids By The Sink: Washing Their Hands Of Mediocrity With New Album
               </p>
+            </div>
             </Link>
           </div>
           <ul className="landing-sub-features">
             <li className="sub-feat1">
               <Link to={`/users/${featureds[3]}`}>
-              <img src="https://s3.us-east-2.amazonaws.com/bandom-dev/featured-artist-photos/main-feature.jpg"
-                className="landing-subfeat1-div"></img>
-                <p className="subfeat1-headline">
-                  New To The Scene: Llama Go-Kart
-                </p>
+                <div className="subfeat-inner">
+                <img src="https://s3.us-east-2.amazonaws.com/bandom-dev/featured-artist-photos/main-feature.jpg"
+                  className="subfeat-img"></img>
+                <p className="subfeat-headline">
+                    New To The Scene: Llama Go-Kart
+                  </p>
+                </div>
               </Link>
             </li>
             <li className="sub-feat2">
               <Link to={`/users/${featureds[0]}`}>
-                <img src="https://s3.us-east-2.amazonaws.com/bandom-dev/featured-artist-photos/sub-feature3.jpg"
-                  className="landing-subfeat2-div"></img>
-                <p className="subfeat2-headline">
-                  The Uncertified Lifeguards Rescue Alternative
-                </p>
+                <div className="subfeat-inner">
+                  <img src="https://s3.us-east-2.amazonaws.com/bandom-dev/featured-artist-photos/sub-feature3.jpg"
+                    className="subfeat-img"></img>
+                  <p className="subfeat-headline">
+                    The Latest from New York Slush
+                  </p>
+                </div>
               </Link>
             </li>
             <li className="sub-feat3">
               <Link to={`/users/${featureds[2]}`}>
-                <img src="https://s3.us-east-2.amazonaws.com/bandom-dev/featured-artist-photos/sub-feature4.jpg"
-                className="landing-subfeat3-div"></img>
-                <p className="subfeat3-headline">
-                  The Latest from New York Slush
-                </p>
+                <div className="subfeat-inner">
+                  <img src="https://s3.us-east-2.amazonaws.com/bandom-dev/featured-artist-photos/sub-feature4.jpg"
+                  className="subfeat-img"></img>
+                  <p className="subfeat-headline">
+                    The Uncertified Lifeguards Rescue Alternative
+                  </p>
+                </div>
               </Link>
             </li>
           </ul>
