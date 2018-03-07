cba741e7d2ae15faec694b1a8a0684e596f910a2
Max <xamskier@aol.com>
2018-03-07 14:04:17 -0500

    started artist page redesign

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -5,6 +5,9 @@ json.albums do
     @user.albums.each do |album|
       json.set! album.id do
         json.extract! album, :title, :description, :artist_id, :year, :genre, :artwork
+        json.tracks album.tracks do |track|
+          json.partial! '/api/tracks/track', track: track
+        end
       end
     end
   end
--- frontend/components/artist/artist.jsx
+++ frontend/components/artist/artist.jsx
@@ -14,43 +14,42 @@ class Artist extends React.Component {
 
   render() {
     if (this.props.artist && this.props.artist.albums) {
-      const albums = Object.keys(this.props.artist.albums);
+      let albums = Object.keys(this.props.artist.albums);
+      albums = albums.sort(function(a, b) {
+        return a.year - b.year;
+      });
       return (
         <div className="main">
           <div className="album-show-main">
-          <div className="cover-div">
-            <p className="cover-title">{this.props.artist.artistName}</p>
-          </div>
-        <div className="artist-show-body">
-          <div className="artist-info">
-          <img className="artist-pic" src={this.props.artist.image_url}></img>
-          <div className="artist-inner-info">
-          <h2>{this.props.artist.artistName}</h2>
-          <h4 className="location">Hometown: {this.props.artist.location}</h4>
-          </div>
-        </div>
-        <div className="discog-div">
-
-          <h4 className="discog-header">Discography</h4>
-          <div className="artist-discog-div">
-            <ul className="albums-list">
-              {albums.map((id, idx) => (
-                <div className="album-item" key={idx}>
-                  <Link to={`/albums/${id}`}>
-                    <img className="album-artwork-show" src={this.props.artist.albums[id].artwork}></img>
-                  </Link>
-                  <div className="artist-albums-info">
-                    <Link to={`/albums/${id}`}>
-                      <h2>{this.props.artist.albums[id].title}</h2>
-                    </Link>
-                    <p>{this.props.artist.albums[id].year}</p>
-                  </div>
+            <div className="artist-show-body">
+              <div className="artist-info">
+                <div className="artist-inner-info">
+                  <h2 className="artist-name">{this.props.artist.artistName}</h2>
+                  <h4 className="location">Hometown: {this.props.artist.location}</h4>
                 </div>
-              ))}
-            </ul>
+                <img className="artist-pic" src={this.props.artist.image_url}></img>
+              </div>
+              <div className="discog-div">
+                <h4 className="discog-header">Discography</h4>
+                <div className="artist-discog-div">
+                  <ul className="albums-list">
+                    {albums.map((id, idx) => (
+                      <div className="album-item" key={idx}>
+                        <Link to={`/albums/${id}`}>
+                          <img className="album-item-artwork" src={this.props.artist.albums[id].artwork}></img>
+                        </Link>
+                        <div className="album-item-info">
+                          <Link to={`/albums/${id}`}>
+                          <h2>{this.props.artist.albums[id].title}</h2>
+                          </Link>
+                          <p>{this.props.artist.albums[id].year}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </ul>
+                </div>
+            </div>
           </div>
-          </div>
-        </div>
         </div>
         <Footer />
       </div>
